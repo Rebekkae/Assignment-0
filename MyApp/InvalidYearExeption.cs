b238b4672eca167ded5408dbf094ae016123b909
Rebekka Viktoria Mejlhede Eriksen <rebekkameriksen@gmail.com>
2022-09-06 11:22:49 +0200

    InvalidYearExeption added

---
+++ MyApp/InvalidYearExeption.cs
@@ -0,0 +1,8 @@
+public class InvalidYearException : Exception
+{
+    public InvalidYearException() {}
+
+    public InvalidYearException(string message) : base(message) {}
+
+    public InvalidYearException(string message, Exception inner) : base(message, inner) {}
+}
